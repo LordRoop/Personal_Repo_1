1bf9463d652184a4d2ab93c2ab33d04a30c8e23d
lordroop <Jagroopvij@gmail.com>
2022-04-07 09:49:43 -0700

    refactored files and packaged them

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,4 @@
-package com.nighthawk.csa.hacks;
+package src;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,6 @@
+package src
+
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
